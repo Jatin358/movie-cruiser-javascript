727cee2534ee2bfc21ec95f787be3073bdefab6d
jatin <chughjatin83@gmail.com>
2022-05-13 00:30:41 +0530

    Javascript Movie Cruiser

---
+++ moviesList.js
@@ -0,0 +1,64 @@
+let movieList = {
+    'movies' :[
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+        {
+            'id' : 1,
+            'Title': 'Hello Brother',
+            'Duration' : '3 hours',
+            'Rating' : 5
+        },
+    ]
+}
